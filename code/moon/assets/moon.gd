8460e0bb0845f13a9ea4f2f7d166968ccd3091e2
Talib Pierson <piersont@reed.edu>
2021-07-15 17:57:13 -0700

    add assets/moon-health-0.png

--- code/moon/assets/moon.gd
+++ code/moon/assets/moon.gd
@@ -1,14 +1,15 @@
 extends Node2D
 class_name Moon
 
-var moon_textures: Array = [preload("res://moon/assets/moon-health-1.png"),
+var moon_textures: Array = [preload("res://moon/assets/moon-health-0.png"),
+							preload("res://moon/assets/moon-health-1.png"),
 							preload("res://moon/assets/moon-health-2.png"),
 							preload("res://moon/assets/moon-health-3.png"),
 							preload("res://moon/assets/moon-health-4.png"),
 							preload("res://moon/assets/moon-health-5.png")]
 
 # Member variables:
-var health: int = 5
+var health: int = len(moon_textures) - 1
 export var speed: float = 512.0 # px/s
 
 # When Moon enters scene
@@ -17,13 +18,13 @@ func _ready() -> void:
 
 # Called every frame. delta_t is the time since the previous frame
 func _process(delta_t: float) -> void:
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		self.translate(Vector2.UP * speed * delta_t)
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("ui_down"):
 		self.translate(Vector2.DOWN * speed * delta_t)
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		self.translate(Vector2.LEFT * speed * delta_t)
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		self.translate(Vector2.RIGHT * speed * delta_t)
 
 
@@ -39,4 +40,4 @@ func update_health(delta_h: int):
 		print("FAILURE!")
 		get_tree().quit()
 		# TODO: display game over menu
-	$Area2D/Sprite.texture = moon_textures[health - 1]
+	$Area2D/Sprite.texture = moon_textures[health]
