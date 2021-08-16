f7ef62e17dae03a819eaa140500e1c3bdac51d37
Rosemary Pierson <piersont@reed.edu>
2021-08-16 16:26:15 -0700

    fix func _on_Restart_button_down

--- code/menu/assets/menu.gd
+++ code/menu/assets/menu.gd
@@ -16,8 +16,10 @@ func _on_Resume_button_down():
 
 
 func _on_Restart_button_down():
+	# reset Level and Moon
 	get_tree().get_root().get_node("Level")._ready()
 	get_tree().get_root().get_node("Level/Moon")._ready()
+	toggle_pause() # Resume game
 
 func _on_Quit_button_down():
 	get_tree().quit()
